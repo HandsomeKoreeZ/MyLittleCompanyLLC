c1e13a27581bdf50225bbd13a2f9ca8a18d04ead
HandsomeKoreeZ <alexey-gumen@yandex.ru>
2020-03-13 15:23:50 +0100

    Entities

---
+++ src/main/java/Entities/Employee.java
@@ -0,0 +1,131 @@
+package Entities;
+
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "EMPLOYEE", uniqueConstraints = {@UniqueConstraint(columnNames = {"EMP_NO"})})
+public class Employee {
+    private Long empID;
+    private String empNO;
+    private String empName;
+    private String job;
+    private Employee manager;
+    private Date hireDate;
+    private Float salary;
+    private byte[] image;
+
+    private Department department;
+    private Set<Employee> employees = new HashSet<Employee>(0);
+
+    public Employee(){}
+
+    public Employee(Long empID, String empName, String job, Employee manager, Date hireDate, Float salary, Department department) {
+        this.empID = empID;
+        this.empNO = "E"+this.empID;
+        this.empName = empName;
+        this.job = job;
+        this.manager = manager;
+        this.hireDate = hireDate;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    @Id
+    @Column(name = "EMP_ID")
+    public Long getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(Long empID) {
+        this.empID = empID;
+    }
+
+    @Column(name = "EMP_NO", length = 20, nullable = false)
+    public String getEmpNO() {
+        return empNO;
+    }
+
+    public void setEmpNO(String empNO) {
+        this.empNO = empNO;
+    }
+
+    @Column(name = "EMP_NAME",length = 50, nullable = false)
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    @Column(name = "JOB",length = 30,nullable = false)
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MNG_ID")
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    @Column(name = "HIRE_DATE", nullable = false)
+    @Temporal(TemporalType.DATE)
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    @Column(name = "SALARY", nullable = false)
+    public Float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Float salary) {
+        this.salary = salary;
+    }
+
+    @Column(name = "IMAGE", length = 1111111, nullable = true)
+    @Lob
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DEPT_ID", nullable = false)
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "empID")
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+}
---
+++ src/main/java/Entities/TimeKeeper.java
@@ -0,0 +1,60 @@
+package Entities;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "TIMEKEEPER")
+public class TimeKeeper {
+    public static final char IN = 'I';
+    public static final char OUT = 'O';
+
+    private String timekeeperId;
+    private Date dateTime;
+    private Employee employee;
+
+    private char IO;
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "Timekeeper_id", length = 36)
+    public String getTimekeeperId(){
+        return timekeeperId;
+    }
+
+    public void setTimekeeperId(String timekeeperId){
+        this.timekeeperId = timekeeperId;
+    }
+
+    @Column(name = "Date_Time", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "EMP_ID", nullable = false)
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Column(name = "In_Out", nullable = false, length = 1)
+    public char getIO() {
+        return IO;
+    }
+
+    public void setIO(char IO) {
+        this.IO = IO;
+    }
+}
